543c6c2cbee7d59a7e5e41224b8eed5cb4e16e1b
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2024-10-14 21:40:04 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary1
+{
+    public class Student
+    {
+        public string Id { set; get; }
+        public string Name { set; get; }
+        public int Age { set; get; }
+
+        public Student(string id, string name, int age) {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public string getStudent()
+        {
+            return $"{Id} : {Name} ({Age})";
+        }
+    }
+}
