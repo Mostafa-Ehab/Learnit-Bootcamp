f15b52725b063ae9faad23b47ce7629580b16129
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2024-10-16 19:26:38 +0300

    Add project files.

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    struct HireDate
+    {
+        public int Year { get; set;  }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public HireDate(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+
+        public HireDate(string date)
+        {
+            string[] datePart = date.Split("/");
+
+            Day = int.Parse(datePart[0]);
+            Month = int.Parse(datePart[1]);
+            Year = int.Parse(datePart[2]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+    }
+    internal class Employee
+    {
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public HireDate HireDate { get; set; }
+        public string Gender { get; set; }
+        public Employee(int id, string name, int salary, HireDate hireDate, string gender)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+        }
+
+        public Employee(int id, string name, HireDate hireDate, string gender): this(id, name, 0, hireDate, gender) {}
+
+        public override string ToString()
+        {
+            string txt = "";
+
+            txt += $"{Id} - {Name} - ({Gender})\n";
+            txt += $"Salary: {Salary}\n";
+            txt += $"Date: {HireDate}\n";
+
+            return txt ;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        //Employee e = new(1, "Mostafa Ehab", new HireDate(2004, 12, 13), "Male");
+        Console.Write("Enter the number of employees: ");
+        int num = int.Parse(Console.ReadLine() ?? "1");
+
+        Employee[] emp = new Employee[num];
+
+        for (int i = 0; i < num; i++)
+        {
+
+            Console.Write("Enter employee name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter salary: ");
+            int salary = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.Write("Enter hiring date: ");
+            HireDate hireDate = new HireDate(Console.ReadLine());
+
+            Console.Write("Enter employee gender: ");
+            string gender = Console.ReadLine();
+
+            emp[i] = new Employee(i, name, salary, hireDate, gender);
+        }
+
+        //sortEmployees(emp);
+
+        for (int i = 0; i <  num; i++)
+        {
+            Console.Write(emp[i].ToString());
+        }
+
+        return 0;
+    }
+
+    static void sortEmployees(Employee[] emp)
+    {
+        throw new NotImplementedException();
+    }
+}
